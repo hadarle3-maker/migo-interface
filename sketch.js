23322439dcdf37a2513f209d14a378ca05d802af
hadarle3-maker <hadarle3@ac.sce.ac.il>
2026-07-13 17:13:43 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let videos = {};
 // סאונד חיצוני ל־Voice Scan
 let voiceScanSound;
 const VOICE_SCAN_SOUND_SRC = "assets/sound/sound_scan.mp3";
-const VOICE_SCAN_SOUND_VOLUME = 0.35;
+const VOICE_SCAN_SOUND_VOLUME = 0.25;
 
 let currentScene = "logoLoop";
 let firstFrameShown = false;
